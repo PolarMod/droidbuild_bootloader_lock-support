bfa805ac4f4f6fce66f2db8a0fefbdea8976fb6a
Andrewerr <p01ar@p01ar.net>
2022-02-06 13:20:53 +0200

    [bootloader][droidbuild][bootloader-lock]: Initial commit

---
+++ Droidbuild.sh
@@ -0,0 +1,12 @@
+include_if_exists "$BASEDIR/incremental_vars.sh"
+
+droidbuild(){
+  if ndef BOOTLOADER_LOCK_SUPPORT; then
+     info "Patching bootloader locking support"
+     exec "patch $BASEDIR/build/core $LOCAL_PATH/patches/Makefile-fix-18.1.patch" 
+     success "Patched bootload lock support"
+     echo "BOOTLOADER_LOCK_SUPPORT=1" >> $BASEDIR/incremental_vars.sh
+  else
+     success "Bootloader locking support is already present"
+  fi
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Bootloader locking support
+## Abstract
+Droidbuild module to patch LineageOS Makefile in order to enable AVBv2 and bootloader locking support.
+## Credits
+The patch being used was provided by [WundermentOS](https://github.com/Wunderment) on GitHub.
