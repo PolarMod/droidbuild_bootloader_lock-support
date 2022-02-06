2443fdbd7a25145d7fba0dda675ec7ebb08f73c2
Andrewerr <p01ar@p01ar.net>
2022-02-06 16:19:23 +0200

    [bootloader-lock][droidbuild]: Droidbuild fixes

--- Droidbuild.sh
+++ Droidbuild.sh
@@ -1,7 +1,7 @@
 include_if_exists "$BASEDIR/incremental_vars.sh"
 
 droidbuild(){
-  LOCAL_PATH = $(dirname $BASH_SOURCE)
+  LOCAL_PATH=$(dirname $BASH_SOURCE)
   if ndef BOOTLOADER_LOCK_SUPPORT; then
      info "Patching bootloader locking support"
      exec "patch $BASEDIR/build/core $LOCAL_PATH/patches/Makefile-fix-18.1.patch" 
